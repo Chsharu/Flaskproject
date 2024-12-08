a4e5f574f6d46b273c24c329c3f52926f9a1bdef
sharanya <sharanyachanda99@gmail.com>
2024-12-07 23:26:51 -0500

    created  your changes here

--- app.py
+++ app.py
@@ -1,83 +1,90 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
-class Book(db.Model):
+class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(200), nullable=False)
-    author = db.Column(db.String(100), nullable=False)
-    genre = db.Column(db.String(100), nullable=False)
-    publication_date = db.Column(db.DateTime, nullable=False)
-    price = db.Column(db.Float, nullable=False)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
-        return f'<Book {self.title}>'
+        return '<Task %r>' % self.id
 
-# Home route (Landing page)
-@app.route('/')
-def home():
-    return render_template('home.html')
+@app.route('/', methods=['GET'])
+def landing_page():
+    return render_template('landing.html')
 
-# Books listing page (View books)
-@app.route('/books', methods=['GET'])
-def books():
-    books = Book.query.all()  # Get all books from the database
-    return render_template('books.html', books=books)
-
-# Add a new book
-@app.route('/add_book', methods=['GET', 'POST'])
-def add_book():
+@app.route('/assignment/', methods=['POST', 'GET'])
+def index():
     if request.method == 'POST':
-        title = request.form['title']
-        author = request.form['author']
-        genre = request.form['genre']
-        publication_date = datetime.strptime(request.form['publication_date'], '%Y-%m-%d')
-        price = float(request.form['price'])
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
 
-        new_book = Book(title=title, author=author, genre=genre, publication_date=publication_date, price=price)
-        db.session.add(new_book)
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/assignment/')
+        except:
+            return 'There was an issue adding your task'
+
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('assignment/index.html', tasks=tasks)
+
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
         db.session.commit()
+        return redirect('/assignment/')
+    except:
+        return 'There was a problem deleting that task'
 
-        return redirect('/books')
-
-    return render_template('add_book.html')
-
-# Update a book
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
-    book = Book.query.get_or_404(id)
+    task = Todo.query.get_or_404(id)
 
     if request.method == 'POST':
-        book.title = request.form['title']
-        book.author = request.form['author']
-        book.genre = request.form['genre']
-        book.publication_date = datetime.strptime(request.form['publication_date'], '%Y-%m-%d')
-        book.price = float(request.form['price'])
+        task.content = request.form['content']
 
         try:
             db.session.commit()
-            return redirect('/books')
+            return redirect('/assignment/')
         except:
-            return 'There was an issue updating the book'
+            return 'There was an issue updating your task'
 
-    return render_template('update_book.html', book=book)
+    else:
+        return render_template('assignment/update.html', task=task)
 
-# Delete a book
-@app.route('/delete/<int:id>')
-def delete(id):
-    book_to_delete = Book.query.get_or_404(id)
+# This route renders created.html page
+@app.route('/assignment/created', methods=['GET'])
+def created_page():
+    return render_template('assignment/created.html')
 
-    try:
-        db.session.delete(book_to_delete)
-        db.session.commit()
-        return redirect('/books')
-    except:
-        return 'There was an issue deleting that book'
+# This route handles tasks specifically for the created page
+@app.route('/created', methods=['POST', 'GET'])
+def created_tasks():
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/created')  # Redirect to the same page after task creation
+        except:
+            return 'There was an issue adding your task'
+
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()  # Query tasks from the database
+        return render_template('created.html', tasks=tasks)  # Render the created.html page
 
 if __name__ == "__main__":
     app.run(debug=True)
