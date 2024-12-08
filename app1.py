2cca75b35df0e3eb223ca6c146561638e3a9914c
sharanya <sharanyachanda99@gmail.com>
2024-12-07 19:25:06 -0500

    booksapplication

--- app1.py
+++ app1.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.db'  # Changed database name
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 
 class Book(db.Model):
@@ -17,37 +18,45 @@ class Book(db.Model):
 
     def __repr__(self):
         return f'<Book {self.title}>'
+@app.route('/', methods=['GET'])
+def landing_page():
+    return render_template('landing.html')  # Display landing page
 
-# Home route (Landing page)
-@app.route('/')
-def home():
-    return render_template('home.html')
-
-# Books listing page (View books)
-@app.route('/books', methods=['GET'])
+@app.route('/books/', methods=['POST', 'GET'])
 def books():
-    books = Book.query.all()  # Get all books from the database
-    return render_template('books.html', books=books)
-
-# Add a new book
-@app.route('/add_book', methods=['GET', 'POST'])
-def add_book():
     if request.method == 'POST':
         title = request.form['title']
         author = request.form['author']
         genre = request.form['genre']
-        publication_date = datetime.strptime(request.form['publication_date'], '%Y-%m-%d')
+        publication_date = datetime.strptime(request.form['publication_date'], '%Y-%m-%d')  # Date format
         price = float(request.form['price'])
 
         new_book = Book(title=title, author=author, genre=genre, publication_date=publication_date, price=price)
-        db.session.add(new_book)
+
+        try:
+            db.session.add(new_book)
+            db.session.commit()
+            return redirect('/books/')
+        except:
+            return 'There was an issue adding your book'
+
+    else:
+        books = Book.query.order_by(Book.publication_date).all()  # Show all books
+        return render_template('books.html', books=books)  # Render book list page
+
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    book_to_delete = Book.query.get_or_404(id)
+
+    try:
+        db.session.delete(book_to_delete)
         db.session.commit()
+        return redirect('/books/')
+    except:
+        return 'There was a problem deleting that book'
 
-        return redirect('/books')
 
-    return render_template('add_book.html')
-
-# Update a book
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
     book = Book.query.get_or_404(id)
@@ -61,23 +70,11 @@ def update(id):
 
         try:
             db.session.commit()
-            return redirect('/books')
+            return redirect('/books/')
         except:
-            return 'There was an issue updating the book'
+            return 'There was an issue updating your book'
 
-    return render_template('update_book.html', book=book)
-
-# Delete a book
-@app.route('/delete/<int:id>')
-def delete(id):
-    book_to_delete = Book.query.get_or_404(id)
-
-    try:
-        db.session.delete(book_to_delete)
-        db.session.commit()
-        return redirect('/books')
-    except:
-        return 'There was an issue deleting that book'
-
+    return render_template('update_book.html', book=book)  # Render update page for the book
 if __name__ == "__main__":
     app.run(debug=True)
+
